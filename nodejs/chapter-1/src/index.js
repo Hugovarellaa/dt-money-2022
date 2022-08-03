d8896e2dc8cda2358e603c1610dd603a7a9bebd1
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 <hugovarellaa@gmail.com>
2022-08-03 11:13:52 -0300

    Rota create account

--- nodejs/chapter-1/src/index.js
+++ nodejs/chapter-1/src/index.js
@@ -1,4 +1,23 @@
 const express = require("express");
-const app = express();
+const { v4: uuidV4 } = require("uuid");
 
-app.listen(3000, () => console.log("server start on port 3333"));
+const app = express();
+app.use(express.json())
+
+const customers = [];
+
+app.post("/account", (req, res) => {
+  const { name, cpf } = req.body;
+
+  const user = {
+    id: uuidV4(),
+    name,
+    cpf,
+    statement: [],
+  };
+
+  customers.push(user);
+  res.status(201).json(user);
+});
+
+app.listen(3333, () => console.log("server start on port 3333"));
